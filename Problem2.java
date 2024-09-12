2268d96f4084dfc472ce8919166250ed528cba89
Iamashitt <bribo60322@gmail.com>
2024-09-12 11:20:14 -0700

    hh

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,24 @@
+/*
+Write a program which takes two doubles and then prints the sum of the numbers when they are both rounded to their nearest whole number. You may assume the doubles are always positive.
 
+Hint: Remember not to just truncate the number - look back at the slides from the lesson if you don't remember how to ensure the number is rounded, not just truncated.
+
+Sample run:
+```
+Two original numbers:
+57.3934
+22.5211
+Answer: 57 + 23 = 80
+```
+*/
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    double d = 57.3934;s
+    double n = 22.5211;
+    int wholenum1 = ;
+    int wholnum2 = ;
+  
+  }
+}
